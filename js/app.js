e43662c4920dd1634ed7e1e4245b4c97820593d4
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-20 13:25:42 +0200

    version 3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 
-function getRandomArticle () {
-	$.getJSON("https://en.wikipedia.org/wiki/Special:Random", function(json){
+function search () {
+	var inputText = document.getElementById('searchTextField').value;
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
+		console.log(json);
+
+		var content = "";
+			
+		for (var i = 1; i < json.length; i++) {
+			for (var a = 0; a < 10; a++) {
+				content += creatLink(json.list[i][a]);
+			}
+		}
+		
+		$('#links').html(content);
 	});
 }
 
-document.getElementById("searchTextField").onclick = getRandomArticle;
+search();
+
+function creatLink(x){
+	var newDiv =
+					'<div class="col-md-12">' +
+						'<div class="col-md-8">' +
+							'<div class="title"><h3>'+ x +'</h3></div>' +
+							'<div class="description"><p>'+ x +'</p></div>' +
+						'</div>' +
+					'</div>' ;
+	return newDiv;
+}
+
+
+
