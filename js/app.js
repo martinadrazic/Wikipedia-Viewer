f4e72d0195dda901a27496be2004a9f6f6d72c6f
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-20 14:39:52 +0200

    version 5

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
+var inputText =
 
 function search () {
-	var inputText = document.getElementById('searchTextField').value;
+	inputText = document.getElementById('searchTextField').value;
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
 		console.log(json);
 
 		var content = "";
 			
-		for (var i = 1; i < json.length - 2; i++) {
-			for (var a = 0; a < json[i].length; a++) {
-				content += creatLink();
-			}
+		for (var a = 0; a < json.length; a++) {
+			content += creatLink(a);
 		}
 		
 		$('#links').html(content);
-
-		function creatLink(){
-			var newDiv =
-							'<div class="col-md-12">' +
-								'<div class="col-md-8">' +
-									'<a href="'+json[3][a]+'" target="_blanck"><h3 class="title">'+ json[i][a] +'</h3>' +
-									'<p class="description">'+ json[i + 1][a] +'</p></a>' +
-								'</div>' +
-							'</div>' ;
-			return newDiv;
-		}
 	});
 }
 
 search();
+
+function creatLink (x) {
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
+		var newDiv =
+					'<div class="col-md-12">' +
+						'<div class="col-md-8 search-list">' +
+							'<a href="'+json[3][x]+'" target="_blanck"><h3 class="title">'+ json[1][x] +'</h3>' +
+								'<p class="description">'+ json[2][x] +'</p></a>' +
+						'</div>' +
+					'</div>' ;
+		return newDiv;
+	});
+}
+
+
 
 
 
