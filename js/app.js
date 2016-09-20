8c6a91836ce056c48758305b0ddc5ab75c4a421f
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-20 15:17:27 +0200

    version 6

--- js/app.js
+++ js/app.js
@@ -3,20 +3,19 @@ var inputText
 function search () {
 	inputText = document.getElementById('searchTextField').value;
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
-		console.log(json);
+		console.log(json[1]);
 
 		var content = "";
 			
-		for (var i = 0; i < json.length + 5; i++) {
-			content += creatLink(i);
+		for (var i = 0; i < json.length; i++) {
+			content += creatLink(json, i);
 		}
 		
 		$('#links').html(content);
 	});
 }
 
-function creatLink (x) {
-	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
+function creatLink (json, x) {
 		var newDiv =
 					'<div class="col-md-12">' +
 						'<div class="col-md-8 search-list">' +
@@ -25,7 +24,6 @@ function creatLink (x) {
 						'</div>' +
 					'</div>' ;
 		return newDiv;
-	});
 }
 
 search();
