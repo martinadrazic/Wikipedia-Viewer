83e53c101cbe16438e813294daa118ef6c9d75bd
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-20 18:27:12 +0200

    version 7

--- js/app.js
+++ js/app.js
@@ -3,27 +3,33 @@ var inputText
 function search () {
 	inputText = document.getElementById('searchTextField').value;
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
-		console.log(json[1]);
+		console.log(json);
 
 		var content = "";
 			
-		for (var i = 0; i < json.length; i++) {
-			content += creatLink(json, i);
+		for (var i = 1; i < json.length - 2; i++) {
+			for (var a = 0; a < json[i].length; a++) {
+			content += creatLink(json, i, a);
+			}
 		}
 		
 		$('#links').html(content);
 	});
 }
 
-function creatLink (json, x) {
-		var newDiv =
-					'<div class="col-md-12">' +
-						'<div class="col-md-8 search-list">' +
-							'<a href="'+json[3][x]+'" target="_blanck"><h3 class="title">'+ json[1][x] +'</h3>' +
-								'<p class="description">'+ json[2][x] +'</p></a>' +
-						'</div>' +
-					'</div>' ;
-		return newDiv;
+function creatLink (json, x, y) {
+
+	if ( json[x + 1][y] === "" ) {
+		json[x + 1][y] = "For more text click here.";
+	}
+
+	var newDiv =
+				'<div class="col-md-12">' +
+					'<div class="col-md-8 search-list">' +
+						'<a href="'+json[3][y]+'" target="_blanck"><h3 class="title">'+ json[x][y] +'</h3>' +'<p class="description">'+ json[x + 1][y] +'</p></a>' +
+					'</div>' +
+				'</div>' ;
+	return newDiv;
 }
 
 search();
