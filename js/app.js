190072004ff80db9c375e7d7f1ee671165ca0d0a
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-21 14:19:39 +0200

    version 8

--- js/app.js
+++ js/app.js
@@ -1,41 +1,50 @@
-var inputText 
+var inputText;
 
-function search () {
+function search() {
 	inputText = document.getElementById('searchTextField').value;
-	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+inputText+"&namespace=0&format=json", function(json){
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=" + inputText + "&namespace=0&format=json", function(json) {
 		console.log(json);
 
 		var content = "";
-			
-		for (var i = 1; i < json.length - 2; i++) {
-			for (var a = 0; a < json[i].length; a++) {
-			content += creatLink(json, i, a);
-			}
+
+		for (var i = 0; i < json[1].length; i++) {
+			content += createText(json, i);
 		}
-		
-		$('#links').html(content);
+		if (content === "") {
+			content = "There were no results matching this search text.";
+		}
+
+		$('#searchedText').html(content);
 	});
 }
 
-function creatLink (json, x, y) {
+function createText(json, x) {
 
-	if ( json[x + 1][y] === "" ) {
-		json[x + 1][y] = "For more text click here.";
+	if (json[2][x] === "") {
+		json[2][x] = "For more text click here.";
 	}
 
 	var newDiv =
-				'<div class="col-md-12">' +
-					'<div class="col-md-8 search-list">' +
-						'<a href="'+json[3][y]+'" target="_blanck"><h3 class="title">'+ json[x][y] +'</h3>' +'<p class="description">'+ json[x + 1][y] +'</p></a>' +
-					'</div>' +
-				'</div>' ;
+		'<div class="col-md-12">' +
+		'<div class="col-md-8 search-list">' +
+		'<a href="' + json[3][x] + '" target="_blanck"><h3 class="title">' + json[1][x] + '</h3>' + '<p class="description">' + json[2][x] + '</p></a>' +
+		'<div class="delimiter"></div>' +
+		'</div>' +
+		'</div>';
 	return newDiv;
 }
 
-search();
+document.getElementById("searchTextField")
+	.addEventListener("keyup", function(event) {
+		// event.preventDefault();
+		if (event.keyCode == 13) {
+			document.getElementById("searchButton").click();
+		}
+	});
 
-
-
-
-
-
+function onkeypressed(evt, input) {
+	var code = evt.charCode || evt.keyCode;
+	if (code == 27) {
+		input.value = '';
+	}
+}
